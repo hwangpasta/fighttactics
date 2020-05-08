b3b5bedf752fc0c826f650291f4f2b32932480a9
hwangpasta <m210@umbc.edu>
2020-05-08 19:41:39 -0400

    Testing making fixtures; got something to work somehow

--- mysite/tft/models.py
+++ mysite/tft/models.py
@@ -1,6 +1,8 @@
+import json
 from django.db import models
 
 # Create your models here.
 
 class Champion(models.Model):
-    champion_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=254, default='Champion')
+    championId = models.CharField(max_length=254, default='hm')
