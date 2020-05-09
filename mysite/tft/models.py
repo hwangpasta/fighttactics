59ef076ff4b5c7c61621e8ab011b076524387910
hwangpasta <m210@umbc.edu>
2020-05-08 20:55:54 -0400

    Updating json data to be made into fixtures

--- mysite/tft/models.py
+++ mysite/tft/models.py
@@ -1,8 +1,12 @@
-import json
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-
 # Create your models here.
 
 class Champion(models.Model):
     name = models.CharField(max_length=254, default='Champion')
     championId = models.CharField(max_length=254, default='hm')
+    cost = models.IntegerField(default=0)
+    traits = ArrayField(models.CharField(max_length=254), blank=True)
+
+    def __str__(self):
+        return self.name
